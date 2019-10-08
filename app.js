a71a23c77f848c6905718bcd77ab3455f220122b
Armno <monkeyarmno@gmail.com>
2019-10-08 09:49:43 +0700

    it works but i dont know why

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-{
-	run();
-
-	async function run() {
-		console.log('running');
-
-		// load models
-		await faceapi.loadMtcnnModel('./models');
-		await faceapi.loadFaceRecognitionModel('./models');
-
-		const videoElement = document.querySelector('#inputVideo');
-		navigator.getUserMedia(
-			{
-				video: {}
-			},
-			stream => (videoElement.srcObject = stream),
-			error => console.error(error)
-		);
+/* eslint-disable-next-line no-unused-vars */
+async function onPlay() {
+	const videoEl = document.querySelector('#inputVideo');
+	if (videoEl.paused || videoEl.ended || !isFaceDetectionModelLoaded()) {
+		return setTimeout(() => onPlay());
 	}
+
+	const options = getFaceDetectorOptions();
+	const result = await faceapi.detectSingleFace(videoEl, options);
+
+	if (result) {
+		const canvas = document.querySelector('#overlay');
+		const dims = faceapi.matchDimensions(canvas, videoEl, true);
+		faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims));
+	}
+
+	setTimeout(() => onPlay());
 }
+
+async function run() {
+	// load face detection models
+	await faceapi.loadMtcnnModel('./models');
+	await faceapi.loadFaceRecognitionModel('./models');
+
+	// access user's webcam
+	const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+	const videoEl = document.querySelector('#inputVideo');
+	videoEl.srcObject = stream;
+}
+
+// helpers
+function isFaceDetectionModelLoaded() {
+	return !!faceapi.nets.mtcnn.params;
+}
+
+function getFaceDetectorOptions() {
+	return new faceapi.MtcnnOptions({ minFaceSize: 200 });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	run();
+});
